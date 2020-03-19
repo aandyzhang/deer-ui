50112506357a8236e130bd1d6ce09564f20791bb
zhangboyang <zhangboyang@zhangboyangdeMacBook-Pro.local>
2020-03-19 11:05:20 +0800

    fix: 修改select,datepiker样式依赖问题,修改datepicker文件夹名称

--- components/date-picker/style.js
+++ components/date-picker/style.js
@@ -1,5 +1,4 @@
 import "../styles";
 import "./style.less";
 import '../spin/style';
-import '../empty/style';
 import '../input/style'
--- components/index.js
+++ components/index.js
@@ -17,4 +17,4 @@ export { default as Row } from './row';
 export { default as Col } from './col';
 export { default as Layout } from './layout';
 export { default as Select } from './select';
-export { default as DatePicker } from './datePicker';
+export { default as DatePicker } from './date-picker';
